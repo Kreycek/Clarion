3374d95fbbdcb3bc0efdcab2d1db13f0c83ccb77
Ricardo Silva Ferreira <ricardo.ferreira.ext@spms.min-saude.pt>
2025-02-11 19:25:46 +0000

    novas features

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginService } from './login.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, HttpClientModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+
+  constructor(private loginService: LoginService) {}
+
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+  
+  onLogin() {
+    const user = {
+      username: this.username,
+      email: this.email,
+      password: this.password,
+    };
+
+    this.loginService.login(user).subscribe(
+      (response) => {
+        // Salva o token no localStorage após o login
+        const token = response.token;
+        localStorage.setItem('token', token);
+
+        // Valida o token após o login
+        this.loginService.validateTokenAfterLogin(token).subscribe(
+          (validationResponse) => {
+            console.log('Token válido:', validationResponse);
+          },
+          (error) => {
+            console.error('Erro ao validar o token:', error);
+          }
+        );
+      },
+      (error) => {
+        this.errorMessage = error.error || 'Erro ao fazer login!';
+      }
+    );
+  }
 }
+  
+
+
---
+++ src/app/login/login.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  private apiUrl = 'http://localhost:8080/login';  // URL do seu backend para login
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  login(user: { username: string; email: string; password: string }): Observable<any> {
+    return this.http.post(this.apiUrl, user, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+    }).pipe(
+      catchError((error) => {
+        throw new Error('Erro de login: ' + error.message);
+      })
+    );
+  }
+
+  // Método para validar o token logo após o login
+  validateTokenAfterLogin(token: string): Observable<any> {
+    return this.authService.validateToken().pipe(
+        
+      catchError((error) => {
+        throw new Error('Erro na validação do token: ' + error.message);
+      })
+    );
+  }
+}
